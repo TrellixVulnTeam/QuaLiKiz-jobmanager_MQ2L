9eed8249cb693dc50e4b70c3078597128a9646cc
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2016-11-16 17:07:57 +0100

    Syntax cleanup and commenting

--- initialize_megadb.py
+++ initialize_megadb.py
@@ -1,18 +1,22 @@
+"""
+Copyright Dutch Institute for Fundamental Energy Research (2016)
+Contributors: Karel van de Plassche (karelvandeplassche@gmail.com)
+License: CeCILL v2.1
+"""
 import os
-import datetime
 import sys
 import inspect
-from qualikiz_tools.qualikiz_io.qualikizrun import QuaLiKizBatch, QuaLiKizRun
-from qualikiz_tools.qualikiz_io.inputfiles import QuaLiKizPlan
-from qualikiz_tools.machine_specific.slurm import Srun, Sbatch
 import sqlite3
-""" Creates a mini-job based on the reference example """
 import csv
 from collections import OrderedDict
 from copy import deepcopy
-from itertools import product, chain
+from itertools import product
+
+from IPython import embed #pylint: disable=unused-import
 import numpy as np
-from IPython import embed
+
+from qualikiz_tools.qualikiz_io.qualikizrun import QuaLiKizBatch, QuaLiKizRun
+from qualikiz_tools.qualikiz_io.inputfiles import QuaLiKizPlan
 
 # Define some standard paths
 runsdir = 'runs'
@@ -20,14 +24,14 @@ if len(sys.argv) == 2:
     if sys.argv[1] != '':
         runsdir = os.path.abspath(sys.argv[1])
 
-rootdir =  os.path.abspath(
-   os.path.join(os.path.abspath(inspect.getfile(inspect.currentframe())),
-               '../..'))
-print (rootdir)
+rootdir = os.path.abspath(
+    os.path.join(os.path.abspath(inspect.getfile(inspect.currentframe())),
+                 '../..'))
+print(rootdir)
 
 # And initialize the megadb job database
 if os.path.isfile('jobdb.sqlite3'):
-    os.remove('jobdb.sqlite3')
+    raise Exception('Error: jobdb.sqlite3 exists!')
 db = sqlite3.connect('jobdb.sqlite3')
 db.execute('''CREATE TABLE queue (
                 Id             INTEGER PRIMARY KEY
@@ -49,29 +53,35 @@ db.execute('''CREATE TABLE job (
 
 # Define the amount of cores to be used
 ncores = 1152
+
+# Load the scan plan from csv. This is based on the 10D database google sheet
 scan_plan = OrderedDict()
 with open('10D database suggestion.csv') as csvfile:
     reader = csv.reader(csvfile)
-    [next(reader) for x in range(3)]
+    [next(reader) for x in range(3)] #pylint: disable=expression-not-assigned
     for row in reader:
         if row[0] == '':
-           break
+            break
         print(row)
         scan_plan[row[1]] = [float(x) for x in row[4:] if x != '']
-    print ('complete plan:' + str(scan_plan))
+        print('complete plan:' + str(scan_plan))
 
-# Define the smallest 'chunck' of our run; the QuaLiKiz run
-qualikiz_chunck = ['Ati', 'Ate', 'Ane', 'qx']
+# Use the parameters.json as base for our hypercube
 base_plan = QuaLiKizPlan.from_json('./parameters.json')
-base_plan['scan_type'] = 'hyperrect'
+
+
+# Start initializing the scan_dict. Start with kthetarhos
 base_plan['scan_dict'] = OrderedDict()
 kthetarhos = scan_plan.pop('kthetarhos')
 base_plan['xpoint_base']['special']['kthetarhos'] = kthetarhos
 
 
+# Define the smallest 'chunck' of our run; the QuaLiKiz run
+qualikiz_chunck = ['Ati', 'Ate', 'Ane', 'qx']
 for name in qualikiz_chunck:
     base_plan['scan_dict'][name] = scan_plan.pop(name)
 
+# Sort the remaining parameters based on LMS difference from center value
 scan_vip = {}
 scan_vip['epsilon'] = 0.15
 scan_vip['Ti_Te_rel'] = 1
@@ -79,9 +89,10 @@ scan_vip['Zeff'] = 1
 scan_vip['Nustar'] = 1e-3
 scan_vip['smag'] = 1
 for key, item in scan_plan.items():
-   scan_plan[key] = sorted(item, key=lambda x: (x - scan_vip[key])**2)
-   print (scan_plan[key])
+    scan_plan[key] = sorted(item, key=lambda x: (x - scan_vip[key])**2) #pylint: disable=cell-var-from-loop
+    print(scan_plan[key])
 
+# Debugging: Remove all but one value for scan_plan
 #for key, item in scan_plan.items():
 #   if key in  ['Ti_Te_rel', 'Zeff', 'Nustar', 'epsilon'] :
 #      scan_plan[key] = [scan_plan[key][0]]
@@ -90,45 +101,42 @@ for key, item in scan_plan.items():
 # Now, we can put multiple runs in one batch script
 batch_chunck = ['smag']
 batch_variable = OrderedDict()
-#set_item = {}
 for name in batch_chunck:
     batch_variable[name] = scan_plan.pop(name)
-    #set_item[name] = base_plan['xpoint_base'].howto_setitem(name)
     db.execute('''ALTER TABLE job ADD COLUMN ''' + name + ''' REAL''')
-    
+
 
 # We can now make a hypercube over all remaining parameters
 # Lets reverse the list so the priority is correct
 scan_plan = OrderedDict(reversed(list(scan_plan.items())))
-#print ('hypercube over:' + str(*scan_plan.items()))
 
 batchlist = []
 for name in scan_plan:
-    #set_item[name] = base_plan['xpoint_base'].howto_setitem(name)
     db.execute('''ALTER TABLE batch ADD COLUMN ''' + name + ''' REAL''')
 
 # Initialize some database stuff
-insert_job_string = '''INSERT INTO job VALUES (?, ?, ?, ? ''' + str(', ?'*(len(batch_chunck))) + ''')'''
-insert_batch_string = '''INSERT INTO batch VALUES (?, ?, ?, ?, ?, ? ''' + str(', ?'*(len(scan_plan))) + ''')'''
-                                                      
+insert_job_string = ('''INSERT INTO job VALUES (?, ?, ?, ? ''' +
+                     str(', ?'*(len(batch_chunck))) + ''')''')
+insert_batch_string = ('''INSERT INTO batch VALUES (?, ?, ?, ?, ?, ? ''' +
+                       str(', ?'*(len(scan_plan))) + ''')''')
+
 queue_id = 0
 batch_id = 0
 tot = np.product([len(x) for x in scan_plan.values()])
 for i, scan_values in enumerate(product(*scan_plan.values())):
-    print (str(i) + '/' + str(tot))
+    print(str(i) + '/' + str(tot))
     base_plan_parent_copy = deepcopy(base_plan)
     xpoint_base = base_plan_parent_copy['xpoint_base']
     batch_name = ''
     value_list = []
 
-
     scan_point = OrderedDict(zip(scan_plan.keys(), scan_values))
     # Reorder elements in point list to avoid dependencies
     if (any([item in scan_point for item in ['epsilon', 'x', 'rho']]) and
-        'Nustar' in scan_point):
-       scan_point.move_to_end('Nustar')
+            'Nustar' in scan_point):
+        scan_point.move_to_end('Nustar')
     if all([item in scan_point for item in ['Nustar', 'Ti_Te_rel']]):
-       scan_point.move_to_end('Ti_Te_rel')
+        scan_point.move_to_end('Ti_Te_rel')
 
     # Set xpoint_base to scan_point
     for name, value in scan_point.items():
@@ -140,9 +148,10 @@ for i, scan_values in enumerate(product(*scan_plan.values())):
         if not np.isclose(xpoint_base[name], value, rtol=1e-4):
             raise Exception('Setting of ' + name + ' failed!' +
                             'Should be ' + str(value) + ' but is ' + str(xpoint_base[name]))
-    # Now we have our 'base'. Each base has one batch with 10 runs inside
+    # Now we have our 'batchbase'. Each batch has a unique base
+    # of which we will create 10 runs
     joblist = []
-    
+
     for name, values in batch_variable.items():
         for i, value in enumerate(values):
             base_plan_batch_copy = deepcopy(base_plan_parent_copy)
@@ -150,52 +159,45 @@ for i, scan_values in enumerate(product(*scan_plan.values())):
             db.execute(insert_job_string, (batch_id, i, 'prepared', None, value))
             job_name = str(name) + str(value)
             xpoint_base[name] = value
-            job = QuaLiKizRun(os.path.join(rootdir, runsdir, batch_name), job_name, '../../../QuaLiKiz', qualikiz_plan=base_plan_batch_copy)
+            # If you want to run a different binary, change here
+            job = QuaLiKizRun(os.path.join(rootdir, runsdir, batch_name),
+                              job_name, '../../../QuaLiKiz',
+                              qualikiz_plan=base_plan_batch_copy)
             joblist.append(job)
-    batch = QuaLiKizBatch(os.path.join(rootdir, runsdir), batch_name, joblist, ncores, partition='regular', repo='m64')
+    # If you want a different QOS or repo, change here
+    batch = QuaLiKizBatch(os.path.join(rootdir, runsdir), batch_name,
+                          joblist, ncores, partition='regular', repo='m64')
     batch.prepare(overwrite_batch=True)
-    db.execute(insert_batch_string, (batch_id, queue_id, 0, os.path.join(batch.batchsdir, batch.name), 'prepared', None) + scan_values)
+    db.execute(insert_batch_string, (batch_id, queue_id, 0,
+                                     os.path.join(batch.batchsdir, batch.name),
+                                     'prepared', None) + scan_values)
     batchlist.append(batch)
     batch_id += 1
 db.commit()
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    print (l.__class__)
-    print (n.__class__)
-    print (len(l).__class__)
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-n = int(np.ceil(len(batchlist) / 24))
-scan_chunks = chunks(batchlist, n)
-scan_chunks = [x for x in scan_chunks]
-#print (scan_chunks)
 
-#resp = input('generate input? [Y/n]')
-from multiprocessing import Process, Manager
-import multiprocessing as mp
-import pickle
-
-
-
-def generate_input(batch):
-    batch.generate_input()
-
+# Legacy: if we want to do parallel generation of input
+#def chunks(l, n):
+#    """Yield successive n-sized chunks from l."""
+#    for ii in range(0, len(l), n):
+#        yield l[i:i + n]
+#numchunks = int(np.ceil(len(batchlist) / 24))
+#scan_chunks = chunks(batchlist, numchunks)
+#scan_chunks = [x for x in scan_chunks]
+##print (scan_chunks)
+#
+##resp = input('generate input? [Y/n]')
+#from multiprocessing import Process, Manager
+#import multiprocessing as mp
+#import pickle
+#
+#
+#
+#def generate_input(batch):
+#    batch.generate_input()
+#
 #pool = mp.Pool(processes=4)
-#print (pickledlist)
 #pool.map(generate_input, batchlist)
 
+# Legacy: Generate all input. Takes long!
 #for batch in batchlist:
 #   generate_input(batch)
-
-#resp = 'n'
-#if resp == '' or resp == 'Y' or resp == 'y':
-#   for i, batch in enumerate(batchlist):
-#      batch.prepare(overwrite_batch=True)
-#      batch.generate_input()
-#      #batchsdir = batch.batchsdir
-#      #name = batch.name
-#      #batchdir = os.path.join(batchsdir, name)
-#      #dumped_batch = QuaLiKizBatch.from_dir(batchdir)
-#      #print (dumped_batch == batch)
-#      print ('batch ' + str(i+1) + '/' + str(len(batchlist)))
-#    #batch.queue_batch()
