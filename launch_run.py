0b298192346f9a5b0a6e7682f90ea512bf94d976
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2016-11-16 17:29:06 +0100

    Syntax cleanup launch_run

--- launch_run.py
+++ launch_run.py
@@ -1,51 +1,63 @@
+"""
+Copyright Dutch Institute for Fundamental Energy Research (2016)
+Contributors: Karel van de Plassche (karelvandeplassche@gmail.com)
+License: CeCILL v2.1
+"""
 import sqlite3
 import warnings
-from qualikiz_tools.qualikiz_io.qualikizrun import QuaLiKizBatch, QuaLiKizRun
-import subprocess as sp
 from warnings import warn
-from IPython import embed
+import subprocess as sp
+import os
+import tarfile
+import shutil
+
+from IPython import embed #pylint: disable=unused-import
+
+from qualikiz_tools.qualikiz_io.qualikizrun import QuaLiKizBatch
 
 def prepare_input(db, amount, mode='ordered', batchid=None):
     if mode == 'ordered':
-        query = db.execute('''SELECT Id, Path FROM batch WHERE State='prepared' LIMIT ?''', (str(amount),))
+        query = db.execute('''SELECT Id, Path FROM batch WHERE State='prepared'
+                           LIMIT ?''', (str(amount),))
     elif mode == 'random':
-        query = db.execute('''SELECT Id, Path FROM batch WHERE State='prepared' ORDER BY RANDOM() LIMIT ?''', (str(amount),))
+        query = db.execute('''SELECT Id, Path FROM batch WHERE State='prepared'
+                           ORDER BY RANDOM() LIMIT ?''', (str(amount),))
     elif mode == 'specific':
-        query = db.execute('''SELECT Id, Path FROM batch WHERE State='prepared' AND Id=? LIMIT ?''', (batchid, str(amount)))
+        query = db.execute('''SELECT Id, Path FROM batch WHERE State='prepared'
+                           AND Id=? LIMIT ?''', (batchid, str(amount)))
     querylist = query.fetchall()
-    print(querylist)
     for el in querylist:
-        print (el)
+        print('generating input for: ' + str(el))
         batchid = el[0]
-        dir = el[1]
-        #QuaLiKizBatch.generate_input
+        batchdir = el[1]
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            batch = QuaLiKizBatch.from_dir(dir)
+            batch = QuaLiKizBatch.from_dir(batchdir)
         batch.generate_input()
         for i, run in enumerate(batch.runlist):
             if run.inputbinaries_exist():
-                db.execute('''UPDATE Job SET State='inputed' WHERE Batch_id=? AND Job_id=?''',
+                db.execute('''UPDATE Job SET State='inputed'
+                           WHERE Batch_id=? AND Job_id=?''',
                            (batchid, i))
                 db.commit()
             else:
-                raise Exception('Generation of input binaries failed')
+                raise Exception('Error: Generation of input binaries failed')
         db.execute('''UPDATE Batch SET State='inputed' WHERE Id=?''',
                    (batchid,))
         db.commit()
 
 
 def queue(db, amount):
-    query = db.execute('''SELECT Id, Path FROM batch WHERE State='inputed' LIMIT ?''', (str(amount),))
+    query = db.execute('''SELECT Id, Path FROM batch WHERE State='inputed'
+                       LIMIT ?''', (str(amount),))
     querylist = query.fetchall()
-    print(querylist)
     for el in querylist:
-        print(el)
+        print('queueing: ' + str(el))
         batchid = el[0]
-        dir = el[1]
+        batchdir = el[1]
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            batch = QuaLiKizBatch.from_dir(dir)
+            batch = QuaLiKizBatch.from_dir(batchdir)
         jobnumber = batch.queue_batch()
         if jobnumber:
             db.execute('''UPDATE Batch SET State='queued', Jobnumber=? WHERE Id=?''',
@@ -67,12 +79,14 @@ def finished_check(db):
     batch_notdone = 0
     for el in querylist:
         batchid = el[0]
-        dir = el[1]
+        batchdir = el[1]
         jobnumber = el[2]
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            batch = QuaLiKizBatch.from_dir(dir)
-        output = sp.check_output(['sacct', '--brief', '--noheader', '--parsable2', '--job', str(jobnumber)])
+            batch = QuaLiKizBatch.from_dir(batchdir)
+        output = sp.check_output(['sacct',
+                                  '--brief', '--noheader', '--parsable2',
+                                  '--job', str(jobnumber)])
         jobline = output.splitlines()[0]
         __, state, __ = jobline.split(b'|')
         print(state)
@@ -92,7 +106,7 @@ def finished_check(db):
             else:
                 state = 'failed'
             db.execute('''UPDATE Batch SET State=? WHERE Id=?''',
-                           (state, batchid))
+                       (state, batchid))
             db.commit()
         elif state.startswith(b'CANCELLED'):
             for i, run in enumerate(batch.runlist):
@@ -100,11 +114,12 @@ def finished_check(db):
                     db_state = 'success'
                 else:
                     db_state = 'failed'
-                db.execute('''UPDATE Job SET State=? AND Note='CANCELLED' WHERE Batch_id=? AND Job_id=?''',
+                db.execute('''UPDATE Job SET State=? AND Note='CANCELLED'
+                           WHERE Batch_id=? AND Job_id=?''',
                            (db_state, batchid, i))
                 db.commit()
-            db.execute('''UPDATE Batch SET State='cancelled' and Note='TIMEOUT' WHERE Id=?''',
-                           (batchid,))
+            db.execute('''UPDATE Batch SET State='cancelled' and Note='TIMEOUT'
+                       WHERE Id=?''', (batchid,))
             db.commit()
         elif state == (b'TIMEOUT'):
             for i, run in enumerate(batch.runlist):
@@ -112,26 +127,24 @@ def finished_check(db):
                     db_state = 'success'
                 else:
                     db_state = 'failed'
-                db.execute('''UPDATE Job SET State=? AND Note='TIMEOUT' WHERE Batch_id=? AND Job_id=?''',
+                db.execute('''UPDATE Job SET State=? AND Note='TIMEOUT'
+                           WHERE Batch_id=? AND Job_id=?''',
                            (db_state, batchid, i))
                 db.commit()
-            db.execute('''UPDATE Batch SET State='failed' AND Note='TIMEOUT' WHERE Id=?''',
-                           (batchid,))
+            db.execute('''UPDATE Batch SET State='failed' AND Note='TIMEOUT'
+                       WHERE Id=?''', (batchid,))
             db.commit()
-
 
         else:
             batch_notdone += 1
-    print (str(batch_notdone) + ' not done')
+    print(str(batch_notdone) + ' not done')
 
-import os
-import tarfile
-import shutil
 def archive(db, limit):
-    query = db.execute('''SELECT Id, Path, Jobnumber FROM batch WHERE State='netcdfized' LIMIT ?''', (limit, ))
+    query = db.execute('''SELECT Id, Path, Jobnumber FROM batch
+                       WHERE State='netcdfized' LIMIT ?''', (limit, ))
     querylist = query.fetchall()
     for el in querylist:
-        print(el)
+        print('Archiving: ' + str(el))
         batchid = el[0]
         batchdir = el[1]
         batchsdir, name = os.path.split(batchdir)
@@ -142,18 +155,21 @@ def archive(db, limit):
         if os.path.isfile(batchdir + '.tar'):
             shutil.rmtree(batchdir)
             db.execute('''UPDATE Batch SET State='archived' WHERE Id=?''',
-                      (batchid, ))
+                       (batchid, ))
             db.commit()
 
 
 def netcdfize(db, limit):
-    query = db.execute('''SELECT Id, Path, Jobnumber FROM batch WHERE State='success' LIMIT ?''', (limit,))
+    query = db.execute('''SELECT Id, Path, Jobnumber FROM batch
+                       WHERE State='success' LIMIT ?''', (limit,))
     querylist = query.fetchall()
     for el in querylist:
         print(el)
         batchid = el[0]
-        dir = el[1]
-        batch = QuaLiKizBatch.from_dir(dir)
+        batchdir = el[1]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            batch = QuaLiKizBatch.from_dir(batchdir)
         batch.to_netcdf()
         for i, run in enumerate(batch.runlist):
             print('Archiving ' + run.rundir)
@@ -161,7 +177,8 @@ def netcdfize(db, limit):
                 tar.add(run.rundir, arcname=os.path.basename(run.rundir))
             if os.path.isfile(run.rundir + '.tar.gz'):
                 shutil.rmtree(run.rundir)
-                db.execute('''UPDATE Job SET State='archived' WHERE Batch_id=? AND Job_id=?''',
+                db.execute('''UPDATE Job SET State='archived'
+                           WHERE Batch_id=? AND Job_id=?''',
                            (batchid, i))
                 db.commit()
         db.execute('''UPDATE Batch SET State='netcdfized' WHERE Id=?''', (batchid, ))
@@ -175,9 +192,9 @@ def trash(db):
         for el in querylist:
             print(el)
             batchid = el[0]
-            dir = el[1]
+            batchdir = el[1]
             try:
-                shutil.rmtree(dir)
+                shutil.rmtree(batchdir)
             except FileNotFoundError:
                 warn(dir + ' already gone')
             db.execute('''UPDATE Batch SET State='thrashed' WHERE Id=?''', (batchid, ))
@@ -186,17 +203,17 @@ def trash(db):
 
 
 queuelimit = 1
-db = sqlite3.connect('jobdb.sqlite3')
+jobdb = sqlite3.connect('jobjobdb.sqlite3')
 #in_queue = waiting_jobs()
 #print (str(in_queue) + ' jobs in queue. Submitting ' + str(queuelimit-in_queue))
-#prepare_input(db, 1, mode='specific', batchid=20)
-#prepare_input(db, 1, mode='specific', batchid=400)
-#prepare_input(db, 1, mode='specific', batchid=800)
-#prepare_input(db, 1, mode='specific', batchid=1200)
-#queue(db, 4)
-finished_check(db)
-#netcdfize(db, 2)
+#prepare_input(jobdb, 1, mode='specific', batchid=20)
+#prepare_input(jobdb, 1, mode='specific', batchid=400)
+#prepare_input(jobdb, 1, mode='specific', batchid=800)
+#prepare_input(jobdb, 1, mode='specific', batchid=1200)
+#queue(jobdb, 4)
+finished_check(jobdb)
+#netcdfize(jobdb, 2)
 #embed()
-#archive(db, 2)
-#trash(db)
-db.close()
+#archive(jobdb, 2)
+#trash(jobdb)
+jobdb.close()
