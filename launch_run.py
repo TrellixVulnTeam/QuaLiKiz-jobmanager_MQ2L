85cb1223c96e95deafab9c260f05c14294453361
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 09:24:41 +0000

    Adding tarfile member sanitization to extractall()

--- launch_run.py
+++ launch_run.py
@@ -240,7 +240,26 @@ def denetcdfize(db, criteria):
         for tar_gz_file in tar_gz_files:
             with tarfile.open(tar_gz_file, 'r:gz') as tar:
                 print('Untarring ' + tar_gz_file)
-                tar.extractall(path=batchdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=batchdir)
             os.remove(tar_gz_file)
         db.execute('''UPDATE Batch SET State='success' WHERE Id=?''', (batchid, ))
         db.execute('''UPDATE Job SET State='success' WHERE Batch_id=?''', (batchid, ))
